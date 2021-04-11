917ae2b68b52f3ac83c3eeefdffde79baa89f50e
Isha Wakankar <iwakankar@hotmail.com>
2021-04-11 11:44:43 +0530

    Fixed bugs

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var app = express();
 var server = require('http').createServer(app);
 // var io = require('socket-io');
 var io = require('socket.io').listen(server);
-var recastai = require('recastai').default
+// var recastai = require('recastai').default
 //var readline = require('readline-sync');
-var build = new recastai.build('access_token', 'en')
+// var build = new recastai.build('access_token', 'en')
 
 users = [];
 connections = [];
@@ -21,6 +21,11 @@ io.sockets.on('connection', function (socket) {
     connections.push(socket);
     console.log('Connected: %s sockets connected', connections.length);
 
+    socket.on('connect', function(data){
+    	console.log('called connect')
+    	updateCurrentUser();
+    })
+
     socket.on('disconnect', function () {
         users.splice(users.indexOf(socket.username), 1);
         updateUsernames();
@@ -29,13 +34,19 @@ io.sockets.on('connection', function (socket) {
     });
 
     socket.on('send message', function (data) {
-        build.dialog({ type: 'text', content: data }, { conversationId: 'CONVERSATION_ID' })
-            .then(function (res) {
+        // build.dialog({ type: 'text', content: data }, { conversationId: 'CONVERSATION_ID' })
+        //     .then(function (res) {
                 var name=socket.username;
+                console.log('inside message loop');
+                console.log('name: ',name);
                 io.sockets.emit('new message', { msg: '<strong>'+name+'</strong>: '+data });
-                io.sockets.emit('new message', { msg: '<strong>Bot</strong>: '
-                +res.messages[0].content });
-            })
+                // io.sockets.emit('new message', { msg: '<strong>Bot</strong>: '
+                // +res.messages[0].content });
+
+//     .catch((error) => {
+//     console.log(error)
+//     // console.log(error.response.body.errors[0].message)
+// })
     
 });
 
@@ -48,5 +59,9 @@ socket.on('new user', function (data, callback) {
 
 function updateUsernames() {
     io.sockets.emit('get users', users);
+}
+
+function updateCurrentUser() {
+	io.sockets.emit('get current user',socket.username);
 }
 })
